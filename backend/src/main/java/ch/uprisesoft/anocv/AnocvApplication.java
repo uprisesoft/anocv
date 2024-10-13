b23c54137d6132b14634018520baa00778b2ce31
Uprise Software <uprisesoft@gmail.com>
2024-10-13 21:55:56 +0200

    first commit

---
+++ backend/src/main/java/ch/uprisesoft/anocv/AnocvApplication.java
@@ -0,0 +1,13 @@
+package ch.uprisesoft.anocv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnocvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnocvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/uprisesoft/anocv/AnocvApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.uprisesoft.anocv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnocvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
